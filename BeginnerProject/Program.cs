6953018840249ce2d3a00f2e9fd4133faf09107b
Basti <basti.hecek@gmail.com>
2020-04-18 13:43:50 +0200

    Calc finish

--- BeginnerProject/Program.cs
+++ BeginnerProject/Program.cs
@@ -12,7 +12,38 @@ namespace BeginnerProject
 
         static void Main(string[] args)
         {
-                        
+            Program program = new Program();
+            program.Mainmenu();
         }
+
+        public void Mainmenu()
+        {
+            bool mainBool = true;
+            while (mainBool)
+            {
+                Console.WriteLine("Hallo!");
+                Console.WriteLine("(1) Addieren, (2) Subtrahieren, (3) Beenden");
+                string userInput = Console.ReadLine();
+                switch (userInput)
+                {
+                    case "1":
+                        int resultA = rechner.Addieren();
+                        Console.WriteLine($"Ihr Ergebnis ist: {resultA}");
+                        Console.ReadKey();
+                        break;
+                    case "2":
+                        int resultS = rechner.Subtrahieren();
+                        Console.WriteLine($"Ihr Ergebnis ist: {resultS}");
+                        Console.ReadKey();
+                        break;
+                    case "3":
+                        mainBool = false;
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        } 
     }
 }
--- BeginnerProject/Rechner.cs
+++ BeginnerProject/Rechner.cs
@@ -8,10 +8,72 @@ namespace BeginnerProject
 {
     public class Rechner
     {
+        private int numberOne;
+        private int numberTwo;
 
         public void Multiplizieren()
         {
+            Console.WriteLine("H");
+            Console.ReadKey();
+        }
 
+        public int Addieren()
+        {
+            while (true)
+            {
+                Console.WriteLine("Geben sie die erste Zahl ein");
+                string userInput = Console.ReadLine();
+                if (!Int32.TryParse(userInput, out numberOne))
+                {
+                    Console.WriteLine("Gebe eine Zahl ein");
+                    continue;
+                }
+                break;
+            }
+
+            while (true) 
+            { 
+                Console.WriteLine("Geben sie die zweite Zahl ein");
+                string userInput = Console.ReadLine();
+                if (!Int32.TryParse(userInput, out numberTwo))
+                {
+                    Console.WriteLine("Gebe eine Zahl ein");
+                    continue;
+                }
+                break;
+            }
+
+            int result = numberOne + numberTwo;
+            return result;
+        }
+        public int Subtrahieren()
+        {
+            while (true)
+            {
+                Console.WriteLine("Geben sie die erste Zahl ein");
+                string userInput = Console.ReadLine();
+                if (!Int32.TryParse(userInput, out numberOne))
+                {
+                    Console.WriteLine("Gebe eine Zahl ein");
+                    continue;
+                }
+                break;
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Geben sie die zweite Zahl ein");
+                string userInput = Console.ReadLine();
+                if (!Int32.TryParse(userInput, out numberTwo))
+                {
+                    Console.WriteLine("Gebe eine Zahl ein");
+                    continue;
+                }
+                break;
+            }
+
+            int result = numberOne - numberTwo;
+            return result;
         }
     }
 }
