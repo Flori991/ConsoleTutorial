15283f520d3f70cb5bc46bef96a3880c17990987
Basti <basti.hecek@gmail.com>
2020-04-19 12:36:35 +0200

    Street Class create

---
+++ WpfBeginnerProject/Models/Street.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfBeginnerProject.Models
+{
+    class Street
+    {
+        public string name { get; set; }
+        public int preis { get; set; }
+        public int miete { get; set; }
+        public int einkommen { get; set; }
+        public int feldart { get; set; }
+    }
+}
